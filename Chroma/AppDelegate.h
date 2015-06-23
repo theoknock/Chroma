fbf31c6a3ce3a754ca4d97605fb135db259ff181
theoknock <theoknock@gmail.com>
2015-06-23 02:08:45 -0700

    Initial Commit

---
+++ Chroma/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chroma
+//
+//  Created by James Alan Bush on 6/23/15.
+//  Copyright (c) 2015 James Alan Bush. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chroma/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chroma
+//
+//  Created by James Alan Bush on 6/23/15.
+//  Copyright (c) 2015 James Alan Bush. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
